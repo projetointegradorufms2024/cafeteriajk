bc6adb4f80ee289be4613383008e0e92bd48f327
Kenniaf <projetointegradorufms@gmail.com>
2025-04-23 23:50:12 -0300

    Adiciona inserts de produtos e pedidos na cafeteria

--- modelagem_cafeteria.sql
+++ modelagem_cafeteria.sql
@@ -14,3 +14,14 @@ CREATE TABLE pedidos (
     data_pedido DATE NOT NULL,
     FOREIGN KEY (produto_id) REFERENCES produtos(id)
 );
+-- Produtos disponíveis na cafeteria
+INSERT INTO produtos (nome, preco) VALUES
+('Café Expresso', 5.50),
+('Cappuccino', 7.00),
+('Pão de Queijo', 4.00);
+
+-- Pedidos realizados
+INSERT INTO pedidos (produto_id, quantidade, data_pedido) VALUES
+(1, 2, '2025-04-20'),  -- 2 Cafés Expressos
+(2, 1, '2025-04-21'),  -- 1 Cappuccino
+(3, 3, '2025-04-22');  -- 3 Pães de Queijo
